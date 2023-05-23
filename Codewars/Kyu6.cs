c7500d9d87fbe5d72b831e28b1808f14e76d842d
@NanayanGrigory <nanayan.g@routeamgroup.com>
2023-05-23 09:19:13 +0300

    Kyu6 - Stop gninnipS My sdroW!

--- Codewars/Kyu6.cs
+++ Codewars/Kyu6.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection.Metadata;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
 
 namespace Practice.Codewars;
 
@@ -22,7 +24,7 @@ public static class Kyu6
             result += valDegree;
         }
 
-        return value == (int) result;
+        return value == (int)result;
     }
 
     #endregion
@@ -35,6 +37,62 @@ public static class Kyu6
             .GroupBy(x => x)
             .First(x => x.Count() % 2 != 0)
             .Key;
+    }
+
+    #endregion
+
+    #region Multiples of 3 or 5
+
+    public static int Solution(int value)
+    {
+        if (value <= 0)
+        {
+            return 0;
+        }
+
+        List<int> values = new(value);
+
+        for (int i = 0; i < value; i++)
+        {
+            values.Add(i);
+        }
+
+        return values
+            .Where(x => x % 3 == 0
+                        || x % 5 == 0)
+            .Sum();
+    }
+
+    #endregion
+
+    #region Stop gninnipS My sdroW!
+
+    public static string SpinWords(string sentence)
+    {
+        string[] arrWords = sentence.Split();
+
+        if (arrWords.Length == 1)
+        {
+            return string.Concat(arrWords.Single().Reverse());
+        }
+
+        StringBuilder result = new();
+        
+        for (int i = 0; i < arrWords.Length; i++)
+        {
+            result.Append(arrWords[i].Length >= 5
+                ? string.Concat(arrWords[i].Reverse())
+                : arrWords[i]);
+        }
+
+        return string.Join(' ', result.ToString());
+
+        return result.ToString();
+
+        StringBuilder strBuild = new();
+
+        return arrWords.Aggregate(strBuild, (current, value) =>
+            current.Append(value.Length >= 5 ? string.Concat(value.Reverse()) : value)).ToString();
     }
 
     #endregion
