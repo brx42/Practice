04d21919eb98392d134338e65b9fe6029b7e27a7
GrigoryNanayan <nanayan.g@routeamgroup.com>
2023-08-20 00:41:30 +0300

    fix sett

--- Program.cs
+++ Program.cs
@@ -4,4 +4,4 @@ using Practice.LeetCode;
 // Console.WriteLine(Easy.SearchInsert(new []{1, 3, 5, 6}, 2));
 
 
-Console.WriteLine("How can mirrors be real if our eyes aren't real".ToJadenCase());
+Console.WriteLine(Kyu8.DrawStairs(10));
